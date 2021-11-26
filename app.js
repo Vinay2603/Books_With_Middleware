500b26562b2c0e74a8c2b3b9954eec0f9dcfd0ac
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-11-26 18:07:40 +0530

    add

--- app.js
+++ app.js
@@ -6,20 +6,38 @@ const app = express()
 //app.use(express.json())  
 
 //logger not working 
+
 const logger = (permission)=>{
     return (req,res)=>{
        const originalSendFunc = res.send.bind(res)
        res.send = function(body){
            body.name = "vinay"
            console.log(body)
-           return originalSendFunc(bofy)
+           return originalSendFunc(body)
        }
        next()
     }
 }
-app.use(logger)
+//app.use(logger)
+/*
+const authorise = (permission) => {
+    return (req, res, next) => {
+      const originalSendFunc = res.send.bind(res);
+      res.send = function (body) {
+        body.name = "Nrupul Dev";
+        console.log(body); // do whatever here
+        return originalSendFunc(body);
+      };
+      next();
+    };
+  };
+  
+  app.use(authorise)
+*/
 
-app.get("/",(req,res)=>{
+
+app.get("/",logger,(req,res)=>{
+    console.log("svjs")
     res.send({books})
 })
 
